fc4add979330660b37e08390a171f1967862adf6
刘阳 <numbksco@gmail.com>
2018-01-21 22:23:46 +0800

    Fix 28a's indentation

--- src/28a.elm
+++ src/28a.elm
@@ -6,35 +6,35 @@ sortByListLengths = sortBy length
 
 main : Html a
 main =
-    text
-        <| case test of
-            0 ->
-                "Your implementation passed all tests."
+  text
+    <| case test of
+      0 ->
+        "Your implementation passed all tests."
 
-            1 ->
-                "Your implementation failed one test."
+      1 ->
+        "Your implementation failed one test."
 
-            x ->
-                "Your implementation failed " ++ (toString x) ++ " tests."
+      x ->
+        "Your implementation failed " ++ (toString x) ++ " tests."
 
 
 test : Int
 test =
-    List.length
-        <| List.filter ((==) False)
-            [ List.map List.length (sortByListLengths [ [], [ 1 ], (1..2), (1..3), (1..4), (1..5) ])
-                == [ 0, 1, 2, 3, 4, 5 ]
-            , List.map List.length (sortByListLengths [ [] ])
-                == [ 0 ]
-            , List.map List.length (sortByListLengths [ [], [ 1 ], (1..100000), (1..4), (1..3), (1..2) ])
-                == [ 0, 1, 2, 3, 4, 100000 ]
-            , List.map List.length (sortByListLengths [ [ 14 ], [ 15 ], [], [ 1 ], [ 12 ], [ 13 ] ])
-                == [ 0, 1, 1, 1, 1, 1 ]
-            , List.map List.length (sortByListLengths [ [ "a", "b", "c" ], [ "a", "b" ], [ "a" ] ])
-                == [ 1, 2, 3 ]
-            ]
+  List.length
+    <| List.filter ((==) False)
+      [ List.map List.length (sortByListLengths [ [], [ 1 ], (1..2), (1..3), (1..4), (1..5) ])
+        == [ 0, 1, 2, 3, 4, 5 ]
+      , List.map List.length (sortByListLengths [ [] ])
+        == [ 0 ]
+      , List.map List.length (sortByListLengths [ [], [ 1 ], (1..100000), (1..4), (1..3), (1..2) ])
+        == [ 0, 1, 2, 3, 4, 100000 ]
+      , List.map List.length (sortByListLengths [ [ 14 ], [ 15 ], [], [ 1 ], [ 12 ], [ 13 ] ])
+        == [ 0, 1, 1, 1, 1, 1 ]
+      , List.map List.length (sortByListLengths [ [ "a", "b", "c" ], [ "a", "b" ], [ "a" ] ])
+        == [ 1, 2, 3 ]
+      ]
 
 
 (..) : Int -> Int -> List Int
 (..) start end =
-    List.range start end
+  List.range start end
